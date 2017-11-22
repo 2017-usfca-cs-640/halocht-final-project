875eb7b915bafcb36c372e0668dc38af5159716b
Naupaka Zimmerman <naupaka@gmail.com>
2017-11-22 15:26:38 -0800

    Update DESCRIPTION and tests

--- tests/render_rmds.R
+++ tests/render_rmds.R
@@ -1,7 +1,22 @@
 # call rmarkdown on all .Rmd files
 f <- list.files(recursive = TRUE)
+
 rmds <- f[grepl(".Rmd$", f)]
+r_scripts <- f[grepl(".R$", f)]
+
 lapply(rmds, rmarkdown::render)
+
+
+# output lints
+for (rfile in r_scripts) {
+  print(lintr::lint(rfile))
+}
+
+# expect no lints
+for (rfile in r_scripts) {
+  lintr::expect_lint(checks = NULL, file = rfile)
+}
+
 
 # output lints
 for (rmdfile in rmds) {
